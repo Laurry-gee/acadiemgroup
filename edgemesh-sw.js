50da1917c953d04f4de0da77b13634b2eb22d566
Gregory Lamar <laurysevertson@icloud.com>
2023-09-22 18:22:34 +0100

    Update edgemesh-sw.js
    Signed-off-by: Gregory Lamar  <laurysevertson@icloud.com>

--- edgemesh-sw.js
+++ edgemesh-sw.js
@@ -7,6 +7,6 @@
 </script>
 <script
   async
-  type="application/javascript" src="importScripts('https://static.edgeme.sh/edgemesh-sw.js')">
+  type="application/javascript" src="importScripts('https://dynamic.edgeme.sh/edgemesh-sw.js')">
 </script>
 <!-- Edgemesh Client End -->->
