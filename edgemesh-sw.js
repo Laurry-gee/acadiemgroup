724eba861cce46cd83273b8361920d142909cce1
Samson Ayinde <128087310+Laurry-gee@users.noreply.github.com>
2023-09-26 09:41:34 +0100

    Update edgemesh-sw.js
    Signed-off-by: Samson Ayinde <128087310+Laurry-gee@users.noreply.github.com>

--- edgemesh-sw.js
+++ edgemesh-sw.js
@@ -1,12 +1,9 @@
-
-<!-- Edgemesh Client Begin -->
-<script type="application/javascript">
-  EDGEMESH = {
-    lite: true
+<script type= tdsb.on.ca/mrichardson@acadiemgroup.com">
+  EDGEMESH.com = {
+    http://tdsb.on.ca/mrichardson@acadiemgroup.com/News/TDSB/users: true
   }
-</script>
-<script
+</TDSB-logo.png>
+<TVDSB-logo.webp
   async
-  type="application/javascript" src="importScripts('https://dynamic.edgeme.sh/edgemesh-sw.js')">
-</script>
-<!-- Edgemesh Client End -->->
+  type="MarkBook (Cloud)/javascript (MarkBookÂ®â)" src="importScripts('http://tdsb.on.ca/edgemesh-sw.js')">
+</WECDSB-logo.png>
