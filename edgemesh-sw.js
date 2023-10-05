20fd073132dcdbdb76401b8984de205b948d0827
Laurence-severtson <128087310+Laurry-gee@users.noreply.github.com>
2023-10-05 19:45:03 +0100

    Update edgemesh-sw.js
    Signed-off-by: Laurence-severtson <128087310+Laurry-gee@users.noreply.github.com>

--- edgemesh-sw.js
+++ edgemesh-sw.js
@@ -1,4 +1,4 @@
-<script type= "tdsb.on.ca/mrichardson@acadiemgroup.com">
+<script type= "application/stylescripts>
   EDGEMESH.com = {
     http://tdsb.on.ca/mrichardson@acadiemgroup.com/News/TDSB/users: true
   }
